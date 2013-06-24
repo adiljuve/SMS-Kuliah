8bcd3ed32b5f617cf74a2cd42e1929ada107464a
adiljuve <adil.informatics@gmail.com>
2013-06-24 09:54:16 +0700

    base_url changed

--- application/config/config.php
+++ application/config/config.php
@@ -11,7 +11,9 @@
 |	http://example.com/
 |
 */
-$config['base_url']	= "http://localhost/sms/";
+$root  = (isset($_SERVER['HTTPS']) ? "https://" : "http://").$_SERVER['HTTP_HOST'];
+$root .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+$config['base_url']    = $root;
 
 /*
 |--------------------------------------------------------------------------
